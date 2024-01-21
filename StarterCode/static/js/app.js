9baa528295e42d02ff4a9121fa903bae7ee092ad
Juan1teno <juan.tenopala@outlook.com>
2024-01-20 19:12:22 -0600

    challenge updated. charts, menu and demographic info table are working now

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,11 +10,11 @@ data.names.forEach(function(name){
 
 // Function to create plots
 function plots(name_value){
-    let name_a = data.sample.find(name_o => parseInt(name_o.id) == name_value);
-    let hover = name_a[0].otu_labels;
+    let name_a = data.samples.find(name_o => parseInt(name_o.id) == name_value);
+    let hover = name_a.otu_labels;
 
-    let bar_y = name_a[0].otu_ids.map(d => "OTU" + d.toString());
-    let bar_x = name_a[0].sample_values;
+    let bar_y = name_a.otu_ids.map(d => "OTU" + d.toString());
+    let bar_x = name_a.sample_values;
     let bar_a = [{
         type: "bar",
         x: bar_x.slice(0, 10).reverse(),
@@ -23,8 +23,8 @@ function plots(name_value){
         orientation: "h",
     }];
 
-    let bubble_x = name_a[0].otu_ids;
-    let bubble_y = name_a[0].sample_values;
+    let bubble_x = name_a.otu_ids;
+    let bubble_y = name_a.sample_values;
     let bubble_a = [{
         x: bubble_x,
         y: bubble_y,
@@ -34,10 +34,27 @@ function plots(name_value){
             size: bubble_y,
             color: bubble_x
         }
-    }]
+    }];
+
+    // Update dempgraphic information
+    let metadata = data.metadata.find(meta => parseInt(meta.id) === parseInt(name_value));
+    if (metadata){
+        updateDemographicTable(metadata);
+    }
+    
 
     Plotly.newPlot("bar", bar_a);
     Plotly.newPlot("bubble", bubble_a);
+}
+
+// Function to format the demographic information into a table
+function updateDemographicTable(metadata){
+    let tableHtml = '<table class="table table-bordered">';
+    for (let names in metadata) {
+        tableHtml += `<tr><td>${names}</td><td>${metadata[names]}</td></tr>`
+    }
+    tableHtml += '</table>';
+    document.getElementById("sample-metadata").innerHTML = "Demographic info: " + tableHtml;
 }
 
 plots(data.names[0]);
